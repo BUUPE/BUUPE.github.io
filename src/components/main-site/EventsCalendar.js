e6068e64c5d59622a8dd9556b56973bff4758bbc
Vitor Vicente <vitor@bu.edu>
2020-03-08 21:45:53 -0400

    Calendar Backend
    Signed-off-by: Vitor Vicente <vitor@bu.edu>

--- src/components/main-site/EventsCalendar.js
+++ src/components/main-site/EventsCalendar.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment'
@@ -11,35 +9,52 @@ import '../../styles/main-site/calendar.css';
 
 const localizer = momentLocalizer(moment)
 const axios = require('axios');
-const now = new Date()
+
+
 
 class EventsCalendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-		events: 
-		[  
-		  {
-			id: 0,
-			title: 'Info Session 1',
-			allDay: false,
-			start: new Date(2020, 2, 14, 17, 30, 0),
-			end: new Date(2020, 2, 14, 20, 30, 0),
-		  },
-		]
+		eventsRaw: [],
+		eventsFiltered: [],
 	}
   }
+  
+  componentDidMount() {
+	this.getEvents();
+  }
+  
+  getEvents() {
+    axios.get('/api/getEvents').then((res) => {
+      console.log(res.data);
+      this.setState({
+        eventsRaw: res.data,
+      }) 
+    })
+  };
+  
   render () {
-	var d = new Date();
-	var m = d.getMonth();
-	var y = d.getYear();
-	
+    const events = this.state.eventsRaw.map((item, index)=>{
+		let ev = {};
+		ev[index] =
+		{
+		  id: item.id,
+		  title: item.title,
+		  allDay: item.allDay,
+		  start: new Date(item.startYear, item.startMonth-1, item.startDay, item.startHour, item.startMinute, 0, 0),
+		  end: new Date(item.endYear, item.endMonth-1, item.endDay, item.endHour, item.endMinute, 0, 0),
+	    };
+		return ev;
+    });
+	  
+	console.log(events);
     return(
 	  <Container className="calendar">
 		<div>
 		  <Calendar
 			localizer={localizer}
-			events={this.state.events}
+			events={events}
 			startAccessor="start"
 			endAccessor="end"
 			defaultView={Views.MONTH}
