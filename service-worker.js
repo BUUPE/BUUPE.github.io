fee4f4ea2ded925ccc7c44f1e24cf56fecb8786b
vitorvicente <vitor@bu.edu>
2020-07-27 12:24:34 +0000

    Deploying to master from  @ 56d149b171826572205552ae84f694456be6d626 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3374a6670fb5b5d0dd53cc4f8288f91.js"
+  "/precache-manifest.da55432eb65c6fef1772cece906d2d37.js"
 );
 
 self.addEventListener('message', (event) => {
