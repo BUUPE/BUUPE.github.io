ebaf4dca3ea0c9734f2abea5cdc6d54764c9ca03
Vitor Vicente <vitor@bu.edu>
2020-08-06 15:46:50 +0100

    Removing Maintenance Mode Signed-off-by: Vitor Vicente <vitor@bu.edu>

--- src/pages/main-site/Login.js
+++ src/pages/main-site/Login.js
@@ -1,6 +1,8 @@
+import Container from "react-bootstrap/Container";
 import React, { Component } from "react";
 
 import "../../styles/main-site/main.css";
+import LoginForm from "../../components/main-site/LoginForm";
 import { compose } from "recompose";
 import { AuthUserContext, withAuthentication } from "../../api/Session";
 import * as ROUTES from "../../constants/routes";
@@ -11,9 +13,11 @@ class LoginBase extends Component {
       <AuthUserContext.Consumer>
         {(authUser) =>
           authUser ? (
-            <>{this.props.history.push(ROUTES.LANDING)}</>
+            <>{this.props.history.push(ROUTES.PANEL)}</>
           ) : (
-            <>{this.props.history.push(ROUTES.LANDING)}</>
+            <Container>
+              <LoginForm history={this.props.history} />
+            </Container>
           )
         }
       </AuthUserContext.Consumer>
--- src/pages/main-site/Members.js
+++ src/pages/main-site/Members.js
@@ -1,26 +1,19 @@
+
 import React from "react";
 import { withRouter } from "react-router-dom";
 
 import Header from "../../components/main-site/Header";
 import Footer from "../../components/main-site/Footer";
+import MemberList from "../../components/main-site/MemberList";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "../../styles/main-site/main.css";
 
 const Members = () => {
   return (
     <div className="landing">
       <Header />
-	  
-	  <Container>
-	    <Row>
-		  <Col className="title-centered">
-            <h1>This page is currently in maintenance, please bear with us. </h1>
-		  </Col>
-		</Row>
-      </Container>
+
+      <MemberList />
 
       <Footer />
     </div>
