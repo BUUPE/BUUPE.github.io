0345738cdd9516d38007279d577d75fc8737770c
vitorvicente <vitor@bu.edu>
2020-08-09 18:25:57 +0000

    Deploying to master from  @ ac826a442492af44b2c79eaf62aff2dbbdd99f16 🚀

---
+++ precache-manifest.2e3a68c2fdfddd320e0fcc850904d011.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b306e10fd955a0d2ab34b87a5322f35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d3c1e5595928e8af38d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "afb0bb9c986d6b593f24",
+    "url": "/static/css/main.98ab2ad6.chunk.css"
+  },
+  {
+    "revision": "4d3c1e5595928e8af38d",
+    "url": "/static/js/2.a11b23fb.chunk.js"
+  },
+  {
+    "revision": "31a3bb157dfe3a3d9b33c9ab8def895f",
+    "url": "/static/js/2.a11b23fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb0bb9c986d6b593f24",
+    "url": "/static/js/main.4e964337.chunk.js"
+  },
+  {
+    "revision": "ec9032c9920b604fa5ef",
+    "url": "/static/js/runtime-main.e9c50c35.js"
+  },
+  {
+    "revision": "d581b6b8a226f6f624fc1855ba50cc1b",
+    "url": "/static/media/header.d581b6b8.jpg"
+  },
+  {
+    "revision": "1fa3818bc22bfe78ed940f1fefc65d75",
+    "url": "/static/media/logo.1fa3818b.png"
+  },
+  {
+    "revision": "5e504bdcd9edd9ab341b27f6b0b7308c",
+    "url": "/static/media/projects.5e504bdc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f60734060894b2dd5745e302584f370.js"
+  "/precache-manifest.2e3a68c2fdfddd320e0fcc850904d011.js"
 );
 
 self.addEventListener('message', (event) => {
